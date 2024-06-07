2520ff564289ef5179e02f37347c4ebf6e33d9a8
Prem Wannakboth <pwboth092@gmail.com>
2024-06-07 08:11:40 +0700

    add controller and gitignore

--- lib/service/controller.dart
+++ lib/service/controller.dart
@@ -1,19 +1,17 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+import '../widget/url.dart';
 import 'model.dart';
 
 class ApiController {
-  final String apiUrl = 'http://10.195.6.60:5000/api';
-  final String searchUrl = '$apiUrl/search';
-
   static Future<ApiResponse> fetchData(String search, String currency) async {
     final response = await http.post(
       Uri.parse(searchUrl),
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode({
         'search': search,
-        'currency': currency,
+        'currency': currency, 
       }),
     );
 
---
+++ lib/widget/url.dart
@@ -0,0 +1,4 @@
+// base url
+String apiUrl = 'http://10.195.6.60:5000/api';
+
+String searchUrl = '$apiUrl/search';
